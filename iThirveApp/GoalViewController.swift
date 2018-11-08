d95ff0c24d6e8264867fdebb345ead7ec5531423
JJ Sb <jj.schraderbachar@gmail.com>
2018-11-08 15:23:34 -0600

    goal VC

---
+++ iThirveApp/GoalViewController.swift
@@ -0,0 +1,9 @@
+//
+//  GoalViewController.swift
+//  iThirveApp
+//
+//  Created by JJ SchraderBachar on 11/8/18.
+//  Copyright © 2018 JJ SchraderBachar. All rights reserved.
+//
+
+import Foundation
